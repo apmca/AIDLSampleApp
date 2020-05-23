0e99ba2e5e4c39bf363d91bfe6522b9e8f668567
Anand <anandpandey.ap@gmail.com>
2020-05-23 21:51:04 +0530

    Sample Demo Activity Added

---
+++ app/src/main/java/com/example/aidlsampleexample/SampleDemoActivity.java
@@ -0,0 +1,14 @@
+package com.example.aidlsampleexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_demo);
+    }
+}
